ae5a7e8223e3072fc8315058058915f035fc1cd3
selfcac <selfcac@5gbinbox.com>
2019-07-26 02:08:18 +0300

    Sub menu + Message (=Toast)

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,9 +1,12 @@
-import { Icon, Menu } from 'antd'
+import { Icon, Menu , message} from 'antd'
 import React from 'react';
 import { ClickParam } from 'antd/lib/menu';
 
+const {SubMenu} = Menu;
+
+
 export interface MenuProps {
-    updateFunc : (i: number) => void;
+    updateFunc: (i: number) => void;
 }
 
 export interface MenuState {
@@ -11,26 +14,35 @@ export interface MenuState {
 }
 
 export default class MainMenu extends React.Component<MenuProps, MenuState> {
-    
-    state : MenuState  = {
+
+    state: MenuState = {
 
     }
 
-    MyClick =  (param: ClickParam) => {
+    MyClick = (param: ClickParam) => {
         this.props.updateFunc(5);
+        message.info("Updated to 5",5);
     }
 
     render() {
         return (
             <Menu mode="horizontal">
-            <Menu.Item key="One" onClick={this.MyClick}> 
-              <Icon type="mail"></Icon>
-              One sssssssss
-            </Menu.Item>
-            <Menu.Item key="Two"> Two </Menu.Item>
-          </Menu>
+                <Menu.Item key="One" onClick={this.MyClick}>
+                    <Icon type="mail"></Icon>
+                    One sssssssss
+                </Menu.Item>
+                <Menu.Item key="One2" onClick={this.MyClick}>
+                    2222222
+                </Menu.Item>
+                <SubMenu title={<span>"I5"</span>}>
+                    <Menu.ItemGroup>
+                        <Menu.Item>i1</Menu.Item>
+                        <Menu.Item>i222</Menu.Item>
+                    </Menu.ItemGroup>
+                </SubMenu>
+            </Menu>
         );
     }
 
-    
+
 }
