3a4cdfe99c88a8f53b2511c585aaa3438b03c70b
selfcac <selfcac@5gbinbox.com>
2019-12-14 23:54:26 +0200

    starting to try and manage data store

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,11 @@ import { DockTop, DockLeft, DockRight, DockBottom } from './components/DockPanel
 import CenterPanel from './components/CenterPanel';
 import { List, Input, Row, Col } from 'antd';
 import Search from 'antd/lib/input/Search';
+import { DataSource } from './data-components/DataSource';
 
 class App extends React.Component {
+
+  data : DataSource = new DataSource();
 
   state: MyNumberProps = {
     myNumber: 0
@@ -18,27 +21,33 @@ class App extends React.Component {
     this.setState({ myNumber: i });
   }
 
+  componentDidMount() {
+    this.data.addData("Hello");
+    this.data.addData("Hello");
+    
+  }
+
   render() {
     return (
 
       <div className="App" style={{ height: "100vh", width: "100vw" }}>
           <DockTop>
             <MainMenu updateFunc={this.updateNumber} ></MainMenu>
-            <div className="fH">
-              <Row className="fH">
-                <Col span={8} className="fH" >
-                  <div style={{padding: "10px"}} className="fH">
+            <div className="fullHeight">
+              <Row className="fullHeight">
+                <Col span={8} className="fullHeight" >
+                  <div style={{padding: "10px"}} className="fullHeight">
                     <div style={{ height: "50%" }} >
                       <DockTop>
                         <Search placeholder="search domain" onSearch={value => console.log(value)} enterButton />
-                        <List></List>
+                        <List dataSource={this.data.filteredData}></List>
                       </DockTop>
                     </div>
                     <div style={{ background: "red", height: "50%" }}>a</div>
                   </div>
                 </Col>
-                <Col span={8} className="fH"><b>col-8</b></Col>
-                <Col span={8} className="fH"><b>col-8</b></Col>
+                <Col span={8} className="fullHeight"><b>col-8</b></Col>
+                <Col span={8} className="fullHeight"><b>col-8</b></Col>
               </Row>
             </div>
           </DockTop>
---
+++ src/data-components/DataSource.tsx
@@ -0,0 +1,50 @@
+
+type DataType = string;
+type FilterItemFunction = (i: DataType) => boolean;
+type SortFunction = (a: DataType, b: DataType) => number;
+type NotifyFunction = () => void;
+
+
+export class DataSource {
+    
+    private myData : DataType[] = [];
+    private subscribers : Array<NotifyFunction> = [];
+    
+    private lastFilterFunc : FilterItemFunction = (a) => true;
+    private lastSortFunc : SortFunction = (a,b) => 0;
+    filteredData : DataType[]  = [];
+
+    loadData (data: DataType[]) {
+        this.myData = data;
+        this.updateData()
+    }
+
+    addSubscriber(f: NotifyFunction) {
+        this.subscribers.push(f);
+        this.notifySubscribers();
+    }
+
+    addData(n: DataType) {
+        this.myData.push(n);
+        this.updateData()
+    }
+
+    applyFilter(f: FilterItemFunction) {
+        this.lastFilterFunc = f;
+        this.updateData()
+    }
+
+    applySort(f: SortFunction) {
+        this.lastSortFunc = f;
+        this.updateData()
+    }
+
+    private updateData() {
+        this.filteredData = this.myData.filter(this.lastFilterFunc).sort(this.lastSortFunc);
+        this.notifySubscribers()
+    }
+
+    private notifySubscribers() {
+        this.subscribers.forEach(f => f());
+    }
+}
