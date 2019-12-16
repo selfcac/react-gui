1cf12c664025d82826f39a151b739a895be4bbc7
selfcac <selfcac@5gbinbox.com>
2019-12-16 21:51:50 +0200

    Filter Menu with badges and better list TODO: List expanding

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React, { ReactNode } from 'react';
 import './App.css';
 import MainMenu from './components/MainMenu';
-import { MyNumberProps } from './components/SomeForm';
-import { DockTop, DockRight } from './components/DockPanel';
-import { List, Row, Col, Tag, Checkbox, Button, Popover } from 'antd';
+import { DockTop, DockRight, DockLeft } from './components/DockPanel';
+import { List, Row, Col, Tag, Checkbox, Button, Popover, Icon, Badge, Menu, Dropdown } from 'antd';
 import Search from 'antd/lib/input/Search';
 import { DataSource, dsDataType } from './data-components/DataSource';
 import { FullHeightDiv, FullHeightClass as FullHeightClassName } from './components/FullHeight';
+import { ClickParam } from 'antd/lib/menu';
 
-class App extends React.Component {
+export interface MyNumberProps2 {
+  myNumber : number
+  visibleDropDown : boolean
+  filterCount : number
+  filterArray : boolean[]
+}
+
+
+class App extends React.Component<{},MyNumberProps2> {
 
   data : DataSource = new DataSource();
 
-  state: MyNumberProps = {
-    myNumber: 0
+  state: MyNumberProps2 = {
+    myNumber: 0,
+
+    visibleDropDown : false,
+    filterCount : 0,
+    filterArray : [false,false,false],
   }
 
   updateNumber = (i: number) => {
@@ -33,18 +45,51 @@ class App extends React.Component {
     };
   }
 
+  toggleFilterDropDown = () => {
+    this.setState({visibleDropDown: !this.state.visibleDropDown});
+  }
+
   addDomain() {
      // Popup?
   }
 
   renderItem(item: dsDataType, index: number) : ReactNode {
     return (
-      <List.Item actions={[(<a>Edit</a>), (<a>Edit</a>)]}>
-      {item.toString()} <br />
-      <Checkbox disabled={true}>Allowed</Checkbox><Tag>hello</Tag>
+      <List.Item actions={[(<a>Edit</a>), (<a>Edit</a>)]} >
+        <List.Item.Meta
+          title = {item.toString()}
+          description = {(
+                <div><Checkbox disabled={true}>Allowed</Checkbox><Tag>hello</Tag></div>
+            )}
+        />
       </List.Item>
     );
   }
+
+  updateFilter = (i : number) => {
+    let fArray = this.state.filterArray;
+    fArray[i] = !fArray[i];
+    let count = this.state.filterCount;
+    if (fArray[i])
+      count++;
+    else
+      count--;
+    this.setState({filterArray: fArray, filterCount: count})
+  }
+
+  filterMenu = (
+    <Menu>
+      <Menu.Item key={0}>
+        <Checkbox onClick={(e)=>this.updateFilter(0)}>Filter 1</Checkbox>
+      </Menu.Item>
+      <Menu.Item key={1}>
+        <Checkbox onClick={(e)=>this.updateFilter(1)}>Filter 1</Checkbox>
+      </Menu.Item>
+      <Menu.Item key={2}>
+        <Checkbox onClick={(e)=>this.updateFilter(2)}>Filter 1</Checkbox>
+      </Menu.Item>
+    </Menu>
+  );
 
   render() {
     return (
@@ -58,12 +103,23 @@ class App extends React.Component {
                   <FullHeightDiv style={{padding: "10px"}} >
                     <DockTop>
                         <DockRight wrapperStyle={{padding: "10px"}}>
-                          <Button style={{margin: "0 5px"}}>+</Button>
+                          <DockRight>
+                            <DockLeft>
+                            <Button type="primary" style={{marginLeft: "10px", marginRight: "10px"}}>+</Button>
+                            <Dropdown overlay={this.filterMenu} trigger={["hover"]} placement="bottomRight"
+                              visible={this.state.visibleDropDown}>
+                              <Badge count={this.state.filterCount} style={{marginRight: "10px"}}>
+                                <Button onClick={this.toggleFilterDropDown} 
+                                 ><Icon type="filter"/></Button>
+                              </Badge>
+                            </Dropdown>
+                            </DockLeft>
                           <Search 
                           placeholder="search domain" 
                           onSearch={value => this.data.applyFilter(this.getFilterFunc(value))} 
                           /* enterButton  */
                           />
+                          </DockRight>
                         </DockRight>
                         <div className={FullHeightClassName}>
                           <div style={{ height: "50%",  maxHeight: "50%", overflow:  "auto", padding: "0 10px" }} >
