7fa7745063406cdea85c253aa973e332af6288c4
selfcac <selfcac@5gbinbox.com>
2019-12-22 23:21:43 +0200

    nodejs server typescript works, just install + 2 configs (tslint + tsconfig)

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
+
 app.listen(port, err => {
   if (err) {
     return console.error(err);
